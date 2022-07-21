eac5850ff49d9edad682ee4498074569bbaf0f7f
Alexcobra111 <49227965+Alexcobra111@users.noreply.github.com>
2022-07-21 16:36:48 +0800

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,45 @@
+const Orders = [
+        {
+            productName: 'Foldable Mini Drone',
+            productNumber: '85361',
+            paymentStatus: 'Due',
+            shipping: 'Pending'
+        },
+        {
+            productName: 'Larvender KF102 Drone',
+            productNumber: '55945',
+            paymentStatus: 'Refunded',
+            shipping: 'Declined'
+        },
+        {
+            productName: 'Foldable Ultra Drone',
+            productNumber: '98954',
+            paymentStatus: 'Due',
+            shipping: 'Pending'
+        },
+        {
+            productName: 'GPS 4K Drone',
+            productNumber: '71542',
+            paymentStatus: 'Paid',
+            shipping: 'Delivered'
+        },
+        {
+            productName: 'Foldable Mini Drone',
+            productNumber: '85361',
+            paymentStatus: 'Paid',
+            shipping: 'Delivered'
+        },
+        {
+            productName: 'Lozenge  Drone',
+            productNumber: '00482',
+            paymentStatus: 'Paid',
+            shipping: 'Delivered'
+        },
+        {
+            productName: 'Django Macro Drone',
+            productNumber: '98568',
+            paymentStatus: 'Due',
+            shipping: 'Pending'
+        },
+
+]
